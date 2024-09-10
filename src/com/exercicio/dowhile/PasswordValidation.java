9faaafd01677f757463bf65f3cc6110e5b5c21d2
Igor <igormagalhaes.ti@gmail.com>
2024-09-10 00:33:33 -0300

    primeiro commit

--- src/com/exercicio/dowhile/PasswordValidation.java
+++ src/com/exercicio/dowhile/PasswordValidation.java
@@ -5,24 +5,37 @@ import java.util.Scanner;
 
 public class PasswordValidation {
 
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner input = new Scanner(System.in);
-		
-		int password;
-		int loginPassword = 2002;
-		
-		do {
-			password = input.nextInt();
-			if(password != 2002){
-				System.out.println("Senha Invalida!");
-			}
-			
-		}while(password != loginPassword);
-		
-		System.out.println("Acesso Permitido");
-		
-		input.close();
-	}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        
+        /*
+        Escreva um programa que repita a leitura de uma senha até que ela seja válida.
+        Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida".
+        Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" 
+        e o algoritmo encerrado. 
+        Considere que a senha correta é o valor 2002.
+        
+        Entrada:      Saída:
+        2200          Senha Invalida
+        1020          Senha Invalida 
+        2022          Senha Invalida
+        2002          Acesso Permitido
+        */
+        
+        int password;
+        int loginPassword = 2002;
+        
+        do {
+            password = input.nextInt();
+            if (password != 2002) {
+                System.out.println("Senha Invalida!");
+            }
+        } while (password != loginPassword);
+        
+        System.out.println("Acesso Permitido");
+        
+        input.close();
+    }
 
 }
